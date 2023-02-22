5d40e75663d20960ee12548400e7f19fcf56609d
CJ.Chen <imaxstone@outlook.com>
2023-02-23 00:40:45 +0800

    update rcms

--- micro.rcms.httpsutils/src/main/java/com/stone/it/micro/rcms/http/RequestExecute.java
+++ micro.rcms.httpsutils/src/main/java/com/stone/it/micro/rcms/http/RequestExecute.java
@@ -1,5 +1,6 @@
 package com.stone.it.micro.rcms.http;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.commons.codec.Charsets;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -22,113 +23,113 @@ import java.util.Set;
 public class RequestExecute extends RequestClient {
 
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RequestExecute.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(RequestExecute.class);
 
-    /**
-     * 请求执行
-     *
-     * @param client
-     * @param methodType
-     * @param url
-     * @param body
-     * @param header
-     * @param params
-     * @return
-     * @throws Exception
-     */
-    protected static ResponseEntity execute(HttpClient client, RequestType methodType,
-                                            String url, String body,
-                                            Map<String, String> header, Map<String, String> params) throws Exception {
-        // 请求类型
-        LOGGER.info("RCMS_REQUEST_TYPE : " + methodType);
-        // 处理请求参数
-        url = handleUrl(url, params);
-        LOGGER.info("RCMS_REQUEST_URL : " + url);
-        // 获取Base
-        HttpRequestBase requestBase = getBase(methodType, url, body, header);
-        // 执行请求
-        HttpResponse httpResponse = client.execute(requestBase);
-        // 解析数据
-        ResponseEntity responseEntity = new ResponseEntity();
-        LOGGER.info("RCMS_REQUEST_RESPONSE_CODE : " + httpResponse.getStatusLine().getStatusCode());
-        // 设置响应码
-        responseEntity.setCode(String.valueOf(httpResponse.getStatusLine().getStatusCode()));
-        // 解析响应体
-        String response = EntityUtils.toString(httpResponse.getEntity(), Charsets.UTF_8);
-        responseEntity.setBody(response);
-        LOGGER.info("RCMS_REQUEST_RESPONSE_BODY : " + response);
-        if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-            responseEntity.setMessage("success");
-        } else {
-            responseEntity.setMessage("fail");
-        }
-        return responseEntity;
-    }
+	/**
+	 * 请求执行
+	 *
+	 * @param client
+	 * @param methodType
+	 * @param url
+	 * @param body
+	 * @param header
+	 * @param params
+	 * @return
+	 * @throws Exception
+	 */
+	protected static ResponseEntity execute(HttpClient client, RequestType methodType, String url, String body,
+	                                        Map<String, String> header, Map<String, String> params) throws Exception {
+		// 请求类型
+		LOGGER.info("RCMS_REQUEST_TYPE : " + methodType);
+		// 处理请求参数
+		url = buildRequestUrl(url, params);
+		LOGGER.info("RCMS_REQUEST_URL : " + url);
+		// 获取Base
+		HttpRequestBase requestBase = getBase(methodType, url, body, header);
+		LOGGER.info("RCMS_REQUEST_HEADERS : " + JSON.toJSONString(requestBase.getAllHeaders()));
+		// 执行请求
+		HttpResponse httpResponse = client.execute(requestBase);
+		// 解析数据
+		ResponseEntity responseEntity = new ResponseEntity();
+		LOGGER.info("RCMS_REQUEST_RESPONSE_CODE : " + httpResponse.getStatusLine().getStatusCode());
+		// 设置响应码
+		responseEntity.setCode(String.valueOf(httpResponse.getStatusLine().getStatusCode()));
+		// 解析响应体
+		String response = EntityUtils.toString(httpResponse.getEntity(), Charsets.UTF_8);
+		responseEntity.setBody(response);
+		LOGGER.info("RCMS_REQUEST_RESPONSE_BODY : " + response);
+		if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+			responseEntity.setMessage("success");
+		} else {
+			responseEntity.setMessage("fail");
+		}
+		return responseEntity;
+	}
 
-    protected static String handleUrl(String url, Map<String, String> params) throws Exception {
-        StringBuilder uriParams = new StringBuilder();
-        Set<Map.Entry<String, String>> paramSet = params.entrySet();
-        for (Map.Entry<String, String> next : paramSet) {
-            uriParams.append("&").append(next.getKey()).append("=").append(next.getValue());
-        }
-        String paramUri = uriParams.toString();
-        if (!url.contains("?")) {
-            paramUri = paramUri.replaceFirst("&", "?");
-        }
-        LOGGER.info("http request params is : " + paramUri);
-        return url + paramUri;
-    }
+	protected static String buildRequestUrl(String url, Map<String, String> params) throws Exception {
+		StringBuilder uriParams = new StringBuilder();
+		Set<Map.Entry<String, String>> paramSet = params.entrySet();
+		for (Map.Entry<String, String> next : paramSet) {
+			uriParams.append("&").append(next.getKey()).append("=").append(next.getValue());
+		}
+		String paramUri = uriParams.toString();
+		if (!url.contains("?")) {
+			paramUri = paramUri.replaceFirst("&", "?");
+		}
+		return url + paramUri;
+	}
 
-    protected static HttpRequestBase getBase(RequestType methodType, String url, String body, Map<String, String> header) throws UnsupportedEncodingException {
-        HttpRequestBase base = null;
-        switch (methodType) {
-            case POST:
-                base = new HttpPost(url);
-                if (body != null) {
-                    StringEntity httpEntity = new StringEntity(body);
-                    ((HttpPost) base).setEntity(httpEntity);
-                }
-                break;
-            case PUT:
-                base = new HttpPut(url);
-                if (body != null) {
-                    StringEntity httpEntity = new StringEntity(body);
-                    ((HttpPut) base).setEntity(httpEntity);
-                }
-                break;
-            case DELETE:
-                base = new HttpDelete(url);
-                break;
-            case GET:
-                base = new HttpGet(url);
-                break;
-            case PATCH:
-                base = new HttpPut(url);
-                if (body != null) {
-                    StringEntity httpEntity = new StringEntity(body);
-                    ((HttpPatch) base).setEntity(httpEntity);
-                }
-                break;
-            default:
-                break;
-        }
-        // 设置请求头
-        if (base != null && header != null) {
-            setHeader(base, header);
-        }
-        return base;
-    }
+	protected static HttpRequestBase getBase(RequestType methodType, String url, String body,
+	                                         Map<String, String> header) throws UnsupportedEncodingException {
+		HttpRequestBase base = null;
+		switch (methodType) {
+			case POST:
+				base = new HttpPost(url);
+				if (body != null) {
+					StringEntity httpEntity = new StringEntity(body);
+					((HttpPost) base).setEntity(httpEntity);
+				}
+				break;
+			case PUT:
+				base = new HttpPut(url);
+				if (body != null) {
+					StringEntity httpEntity = new StringEntity(body);
+					((HttpPut) base).setEntity(httpEntity);
+				}
+				break;
+			case DELETE:
+				base = new HttpDelete(url);
+				break;
+			case GET:
+				base = new HttpGet(url);
+				break;
+			case PATCH:
+				base = new HttpPut(url);
+				if (body != null) {
+					StringEntity httpEntity = new StringEntity(body);
+					((HttpPatch) base).setEntity(httpEntity);
+				}
+				break;
+			default:
+				break;
+		}
+		// 设置请求头
+		if (base != null && header != null) {
+			setHeader(base, header);
+		}
+		return base;
+	}
 
-    private static void setHeader(HttpRequestBase base, Map<String, String> header) {
-        // 设置默认请求头
-        base.setHeader("Content-Type", "application/json;charset=utf-8");
-        base.setHeader("Accept", "application/json");
-        if (header.size() > 0) {
-            Set<Map.Entry<String, String>> headers = header.entrySet();
-            for (Map.Entry<String, String> next : headers) {
-                base.setHeader(next.getKey(), next.getValue());
-            }
-        }
-    }
+	private static void setHeader(HttpRequestBase base, Map<String, String> header) {
+		// 设置默认请求头
+		base.setHeader("Content-Type", "application/json;charset=utf-8");
+		base.setHeader("Accept", "application/json");
+		if (header.size() > 0) {
+			Set<Map.Entry<String, String>> headers = header.entrySet();
+			for (Map.Entry<String, String> next : headers) {
+				base.setHeader(next.getKey(), next.getValue());
+			}
+		}
+	}
 
 }
--- micro.rcms.httpsutils/src/main/java/com/stone/it/micro/rcms/http/RequestUtil.java
+++ micro.rcms.httpsutils/src/main/java/com/stone/it/micro/rcms/http/RequestUtil.java
@@ -12,77 +12,80 @@ import java.util.Map;
 public class RequestUtil extends RequestExecute {
 
 
-    public static ResponseEntity doGet(String url) throws Exception {
-        return doGet(url, null);
-    }
+	public static ResponseEntity doGet(String url) throws Exception {
+		return doGet(url, null);
+	}
 
-    public static ResponseEntity doGet(String url, Map<String, String> params) throws Exception {
-        return doGet(url, params, null);
-    }
+	public static ResponseEntity doGet(String url, Map<String, String> params) throws Exception {
+		return doGet(url, params, null);
+	}
 
-    public static ResponseEntity doGet(String url, Map<String, String> params, Map<String, String> header) throws Exception {
-        return doGet(getClient(url), url, params, header);
-    }
+	public static ResponseEntity doGet(String url, Map<String, String> params, Map<String, String> header)
+			throws Exception {
+		return doGet(getClient(url), url, params, header);
+	}
 
-    public static ResponseEntity doGet(HttpClient httpClient,
-                                       String url,
-                                       Map<String, String> params,
-                                       Map<String, String> header) throws Exception {
-        return execute(httpClient, RequestType.GET, url, null, header, params);
-    }
+	public static ResponseEntity doGet(HttpClient httpClient,
+	                                   String url,
+	                                   Map<String, String> params,
+	                                   Map<String, String> header) throws Exception {
+		return execute(httpClient, RequestType.GET, url, null, header, params);
+	}
 
-    public static ResponseEntity doPut(String url, String body) throws Exception {
-        return doPut(url, body, null);
-    }
+	public static ResponseEntity doPut(String url, String body) throws Exception {
+		return doPut(url, body, null);
+	}
 
-    public static ResponseEntity doPut(String url, String body, Map<String, String> header) throws Exception {
-        return doPut(getClient(url), url, body, header);
-    }
+	public static ResponseEntity doPut(String url, String body, Map<String, String> header) throws Exception {
+		return doPut(getClient(url), url, body, header);
+	}
 
-    public static ResponseEntity doPut(HttpClient httpClient, String url,
-                                       String body, Map<String, String> header) throws Exception {
-        return execute(httpClient, RequestType.PUT, url, body, header, null);
-    }
+	public static ResponseEntity doPut(HttpClient httpClient, String url,
+	                                   String body, Map<String, String> header) throws Exception {
+		return execute(httpClient, RequestType.PUT, url, body, header, null);
+	}
 
-    /**
-     * Post请求：后一个请求不会把第一个请求覆盖掉。（所以Post用来增资源）
-     *
-     * @param url
-     * @param body
-     * @return
-     * @throws Exception
-     */
-    public static ResponseEntity doPost(String url, String body) throws Exception {
-        return doPost(url, body, null);
-    }
+	/**
+	 * Post请求：后一个请求不会把第一个请求覆盖掉。（所以Post用来增资源）
+	 *
+	 * @param url
+	 * @param body
+	 * @return
+	 * @throws Exception
+	 */
+	public static ResponseEntity doPost(String url, String body) throws Exception {
+		return doPost(url, body, null);
+	}
 
-    public static ResponseEntity doPost(String url, String body, Map<String, String> header) throws Exception {
-        return doPost(getClient(url), url, body, header);
-    }
+	public static ResponseEntity doPost(String url, String body, Map<String, String> header) throws Exception {
+		return doPost(getClient(url), url, body, header);
+	}
 
-    public static ResponseEntity doPost(HttpClient httpClient, String url, String body, Map<String, String> header) throws Exception {
-        return execute(httpClient, RequestType.POST, url, body, header, null);
-    }
+	public static ResponseEntity doPost(HttpClient httpClient, String url, String body, Map<String, String> header)
+			throws Exception {
+		return execute(httpClient, RequestType.POST, url, body, header, null);
+	}
 
-    /**
-     * @param url
-     * @return
-     * @throws Exception
-     */
-    public static ResponseEntity doDelete(String url) throws Exception {
-        return doDelete(url, null);
-    }
+	/**
+	 * @param url
+	 * @return
+	 * @throws Exception
+	 */
+	public static ResponseEntity doDelete(String url) throws Exception {
+		return doDelete(url, null);
+	}
 
-    public static ResponseEntity doDelete(String url, String body) throws Exception {
-        return doDelete(url, body, null);
-    }
+	public static ResponseEntity doDelete(String url, String body) throws Exception {
+		return doDelete(url, body, null);
+	}
 
-    public static ResponseEntity doDelete(String url, String body, Map<String, String> header) throws Exception {
-        return doDelete(getClient(url), url, body, header);
-    }
+	public static ResponseEntity doDelete(String url, String body, Map<String, String> header) throws Exception {
+		return doDelete(getClient(url), url, body, header);
+	}
 
-    public static ResponseEntity doDelete(HttpClient client, String url, String body, Map<String, String> header) throws Exception {
-        return execute(client, RequestType.DELETE, url, body, header, null);
-    }
+	public static ResponseEntity doDelete(HttpClient client, String url, String body, Map<String, String> header)
+			throws Exception {
+		return execute(client, RequestType.DELETE, url, body, header, null);
+	}
 
 }
