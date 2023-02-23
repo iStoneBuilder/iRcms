30a8c826897e2fa1a471c174609d5f782e212d84
CJ.Chen <imaxstone@outlook.com>
2023-02-23 21:57:30 +0800

    update rcms http

--- micro.rcms.httpsutils/src/main/java/com/stone/it/micro/rcms/http/ResponseEntity.java
+++ micro.rcms.httpsutils/src/main/java/com/stone/it/micro/rcms/http/ResponseEntity.java
@@ -1,25 +1,52 @@
 package com.stone.it.micro.rcms.http;
 
-import lombok.Data;
-
 /**
  * @author cj.stone
  * @Date 2023/2/21
  * @Desc
  */
 
-@Data
 public class ResponseEntity {
 
-    private String code;
-    private String message;
-    private String body;
-    private String errors;
+	private String code;
+	private String message;
+	private String body;
+	private String errors;
 
-    public ResponseEntity() {
-        code = "200";
-        message = "success";
-    }
+	public ResponseEntity() {
+		code = "200";
+		message = "success";
+	}
 
+	public String getCode() {
+		return code;
+	}
 
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getErrors() {
+		return errors;
+	}
+
+	public void setErrors(String errors) {
+		this.errors = errors;
+	}
 }
