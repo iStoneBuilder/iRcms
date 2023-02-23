4e13a4bcd612731d9adce24bf121a9763444506f
CJ.Chen <imaxstone@outlook.com>
2023-02-23 21:57:30 +0800

    update rcms http

--- micro.rcms.httpsutils/src/main/java/com/stone/it/micro/rcms/http/RequestUtil.java
+++ micro.rcms.httpsutils/src/main/java/com/stone/it/micro/rcms/http/RequestUtil.java
@@ -12,37 +12,33 @@ import java.util.Map;
 public class RequestUtil extends RequestExecute {
 
 
-	public static ResponseEntity doGet(String url) throws Exception {
+	public static ResponseEntity doGet(String url) {
 		return doGet(url, null);
 	}
 
-	public static ResponseEntity doGet(String url, Map<String, String> params) throws Exception {
+	public static ResponseEntity doGet(String url, Map<String, String> params) {
 		return doGet(url, params, null);
 	}
 
-	public static ResponseEntity doGet(String url, Map<String, String> params, Map<String, String> header)
-			throws Exception {
+	public static ResponseEntity doGet(String url, Map<String, String> params, Map<String, String> header) {
 		return doGet(getClient(url), url, params, header);
 	}
 
-	public static ResponseEntity doGet(HttpClient httpClient,
-	                                   String url,
-	                                   Map<String, String> params,
-	                                   Map<String, String> header) throws Exception {
-		return execute(httpClient, RequestType.GET, url, null, header, params);
+	public static ResponseEntity doGet(HttpClient client, String url, Map<String, String> params,
+	                                   Map<String, String> header) {
+		return execute(client, RequestType.GET, url, null, header, params);
 	}
 
-	public static ResponseEntity doPut(String url, String body) throws Exception {
+	public static ResponseEntity doPut(String url, String body) {
 		return doPut(url, body, null);
 	}
 
-	public static ResponseEntity doPut(String url, String body, Map<String, String> header) throws Exception {
+	public static ResponseEntity doPut(String url, String body, Map<String, String> header) {
 		return doPut(getClient(url), url, body, header);
 	}
 
-	public static ResponseEntity doPut(HttpClient httpClient, String url,
-	                                   String body, Map<String, String> header) throws Exception {
-		return execute(httpClient, RequestType.PUT, url, body, header, null);
+	public static ResponseEntity doPut(HttpClient client, String url, String body, Map<String, String> header) {
+		return execute(client, RequestType.PUT, url, body, header, null);
 	}
 
 	/**
@@ -51,40 +47,38 @@ public class RequestUtil extends RequestExecute {
 	 * @param url
 	 * @param body
 	 * @return
-	 * @throws Exception
+	 * @
 	 */
-	public static ResponseEntity doPost(String url, String body) throws Exception {
+	public static ResponseEntity doPost(String url, String body) {
 		return doPost(url, body, null);
 	}
 
-	public static ResponseEntity doPost(String url, String body, Map<String, String> header) throws Exception {
+	public static ResponseEntity doPost(String url, String body, Map<String, String> header) {
 		return doPost(getClient(url), url, body, header);
 	}
 
-	public static ResponseEntity doPost(HttpClient httpClient, String url, String body, Map<String, String> header)
-			throws Exception {
-		return execute(httpClient, RequestType.POST, url, body, header, null);
+	public static ResponseEntity doPost(HttpClient client, String url, String body, Map<String, String> header) {
+		return execute(client, RequestType.POST, url, body, header, null);
 	}
 
 	/**
 	 * @param url
 	 * @return
-	 * @throws Exception
+	 * @
 	 */
-	public static ResponseEntity doDelete(String url) throws Exception {
+	public static ResponseEntity doDelete(String url) {
 		return doDelete(url, null);
 	}
 
-	public static ResponseEntity doDelete(String url, String body) throws Exception {
+	public static ResponseEntity doDelete(String url, String body) {
 		return doDelete(url, body, null);
 	}
 
-	public static ResponseEntity doDelete(String url, String body, Map<String, String> header) throws Exception {
+	public static ResponseEntity doDelete(String url, String body, Map<String, String> header) {
 		return doDelete(getClient(url), url, body, header);
 	}
 
-	public static ResponseEntity doDelete(HttpClient client, String url, String body, Map<String, String> header)
-			throws Exception {
+	public static ResponseEntity doDelete(HttpClient client, String url, String body, Map<String, String> header) {
 		return execute(client, RequestType.DELETE, url, body, header, null);
 	}
 
