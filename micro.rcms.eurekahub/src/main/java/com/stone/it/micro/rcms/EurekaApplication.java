c6facf9da7e3190c63856e1bdf95dd3b34b26774
CJ.Chen <imaxstone@outlook.com>
2023-04-11 22:21:09 +0800

    profile update

--- micro.rcms.eurekahub/src/main/java/com/stone/it/micro/rcms/EurekaApplication.java
+++ micro.rcms.eurekahub/src/main/java/com/stone/it/micro/rcms/EurekaApplication.java
@@ -1,5 +1,7 @@
 package com.stone.it.micro.rcms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -11,7 +13,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(EurekaApplication.class);
+
   public static void main(String[] args) {
+    LOGGER.info("Eureka Application start ...... ");
     SpringApplication.run(EurekaApplication.class, args);
   }
 
