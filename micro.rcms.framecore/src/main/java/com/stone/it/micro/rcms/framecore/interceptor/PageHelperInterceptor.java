123a006f692dde437de00e5bb2f628d550b5e325
CJ.Chen <imaxstone@outlook.com>
2023-04-26 21:43:01 +0800

    注解学习

---
+++ micro.rcms.framecore/src/main/java/com/stone/it/micro/rcms/framecore/interceptor/PageHelperInterceptor.java
@@ -0,0 +1,12 @@
+package com.stone.it.micro.rcms.framecore.interceptor;
+
+/**
+ * Mybatis 分页插件
+ *
+ * @author cj.stone
+ * @Date 2023/4/26
+ * @Desc
+ */
+public class PageHelperInterceptor {
+
+}
