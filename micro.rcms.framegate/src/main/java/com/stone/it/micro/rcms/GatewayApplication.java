1cec67e5d60c63cc93d0e881260c5be9e9ff88e8
CJ.Chen <imaxstone@outlook.com>
2023-04-27 00:13:23 +0800

    代码格式化

--- micro.rcms.framegate/src/main/java/com/stone/it/micro/rcms/GatewayApplication.java
+++ micro.rcms.framegate/src/main/java/com/stone/it/micro/rcms/GatewayApplication.java
@@ -1,5 +1,7 @@
-package com.stone.it.micro.rcms.gateway;
+package com.stone.it.micro.rcms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -13,7 +15,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class GatewayApplication {
 
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
+
   public static void main(String[] args) {
+    LOGGER.info("Gateway Application server start ...... ");
     SpringApplication.run(GatewayApplication.class, args);
   }
 
---
+++ micro.rcms.rcmflyway/src/main/java/com/stone/it/micro/rcms/FlywayApplication.java
@@ -0,0 +1,15 @@
+package com.stone.it.micro.rcms;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author cj.stone
+ * @Date 2023/4/27
+ * @Desc
+ */
+public class FlywayApplication {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(FlywayApplication.class);
+
+}
