79cf5a93f66a7502964de81589031fa72d183548
CJ.Chen <imaxstone@outlook.com>
2023-04-27 20:06:10 +0800

    Spring Boot 集成 Flyway 实现数据库版本控制

--- micro.rcms.rcmflyway/src/main/java/com/stone/it/micro/rcms/FlywayApplication.java
+++ micro.rcms.rcmflyway/src/main/java/com/stone/it/micro/rcms/FlywayApplication.java
@@ -2,14 +2,22 @@ package com.stone.it.micro.rcms;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author cj.stone
  * @Date 2023/4/27
  * @Desc
  */
+@SpringBootApplication
 public class FlywayApplication {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(FlywayApplication.class);
+
+  public static void main(String[] args) {
+    LOGGER.info("Flyway Application server start ...... ");
+    SpringApplication.run(FlywayApplication.class, args);
+  }
 
 }
