4fe3ceb394e499ddea1d1d15ec15dadc9aa77456
CJ.Chen <imaxstone@outlook.com>
2024-11-05 09:28:57 +0800

    登录接口改造

--- micro.rcms.auth/src/main/java/com/stone/it/rcms/auth/service/impl/AuthLoginService.java
+++ micro.rcms.auth/src/main/java/com/stone/it/rcms/auth/service/impl/AuthLoginService.java
@@ -45,39 +45,53 @@ public class AuthLoginService implements IAuthLoginService {
 
     @Override
     public LoginResVO userLogin(AuthUserVO userVO) {
-        JSONObject loginInfo = subjectLogin(userVO, "account");
+        // 登录认证
+        String sessionId = subjectLogin(userVO.getUserId(), userVO.getPassword(), "account");
+        Calendar expTime = JwtUtils.getExpireTime(60 * 5);
+        String accessToken = buildJwtToken(sessionId, userVO.getUserId(), userVO.getPassword(), "app", expTime);
+        String refreshToken =
+            buildJwtToken(sessionId, userVO.getUserId(), userVO.getPassword(), "app", JwtUtils.getExpireTime(60 * 6));
         LoginResVO loginResVO = new LoginResVO();
-        loginResVO.setAccessToken(loginInfo.getString("accessToken"));
-        loginResVO.setRefreshToken(loginInfo.getString("refreshToken"));
-        AccountVO accountVO = JSON.toJavaObject(loginInfo.getJSONObject("userInfo"), AccountVO.class);
-        loginResVO.setUsername(accountVO.getAccountCode());
-        loginResVO.setNickname(accountVO.getAccountName());
-        String[] roles = (accountVO.getAccountRoles()).split(",");
+        loginResVO.setAccessToken(accessToken);
+        loginResVO.setRefreshToken(refreshToken);
+        // 获取用户信息
+        AccountVO dbUser = authSettingService.getUserInfoByUserId(userVO.getUserId());
+        loginResVO.setUsername(dbUser.getAccountCode());
+        loginResVO.setNickname(dbUser.getAccountName());
+        String[] roles = (dbUser.getAccountRoles()).split(",");
         ArrayList<String> roleList = new ArrayList<>();
         Collections.addAll(roleList, roles);
         loginResVO.setRoles(roleList);
         ArrayList<String> permissions = new ArrayList<String>();
-        if (accountVO.getAccountRoles().contains("platformAdmin")) {
+        if (dbUser.getAccountRoles().contains("platformAdmin")) {
             permissions.add("*:*:*");
         }
         loginResVO.setPermissions(permissions);
-        loginResVO.setExpires(DateUtil.formatDate(loginInfo.getDate("expireTime"), "yyyy-MM-dd HH:mm:ss"));
+        loginResVO.setExpires(DateUtil.formatDate(expTime.getTime(), "yyyy-MM-dd HH:mm:ss"));
         return loginResVO;
     }
 
     @Override
     public JSONObject appToken(AppSecretVO appSecretVO) {
-        AuthUserVO authUserVO = new AuthUserVO();
-        authUserVO.setUserId(appSecretVO.getAppId());
-        authUserVO.setPassword(appSecretVO.getSecret());
-        JSONObject subJson = subjectLogin(authUserVO, "app");
+        String sessionId = subjectLogin(appSecretVO.getAppId(), appSecretVO.getSecret(), "app");
+        if (sessionId == null) {
+            return null;
+        }
         JSONObject result = new JSONObject();
-        Map<String, String> info = JwtUtils.getTokenInfo(subJson.getString("accessToken"));
-        long exp = Long.valueOf(info.get("exp"));
-        info.put("sessionId", subJson.getString("sessionId"));
-        info.remove("exp");
-        result.put("Authorization", JwtUtils.generateTokenDate(info, new Date(exp)));
+        Calendar expTime = JwtUtils.getExpireTime(60 * 30);
+        String accessToken = buildJwtToken(sessionId, appSecretVO.getAppId(), appSecretVO.getSecret(), "app", expTime);
+        result.put("Authorization", accessToken);
+        result.put("expires", DateUtil.formatDate(expTime.getTime(), "yyyy-MM-dd HH:mm:ss"));
         return result;
+    }
+
+    private String buildJwtToken(String sessionId, String account, String password, String type, Calendar instance) {
+        Map<String, String> map = new HashMap<>();
+        map.put("sessionId", sessionId);
+        map.put("account", account);
+        map.put("password", password);
+        map.put("type", type);
+        return JwtUtils.generateToken(map, instance);
     }
 
     @Override
@@ -98,33 +112,25 @@ public class AuthLoginService implements IAuthLoginService {
         }
     }
 
-    private JSONObject subjectLogin(AuthUserVO userVO, String type) {
+    private String subjectLogin(String account, String password, String type) {
         // 查询数据库用户信息
-        AccountVO dbUser = authSettingService.getUserInfoByUserId(userVO.getUserId());
+        AccountVO dbUser = authSettingService.getUserInfoByUserId(account);
         if (dbUser == null) {
             throw new RcmsApplicationException(500, "账号/密码错误！");
         }
-        if (!dbUser.getPassword().equals(userVO.getPassword())) {
+        if (!dbUser.getPassword().equals(password)) {
             throw new RcmsApplicationException(500, "账号/密码错误！");
         }
         if (!dbUser.getAccountType().equals(type)) {
             throw new RcmsApplicationException(500, "账号/密码错误！");
         }
-        JSONObject result = new JSONObject();
         Subject subject = SecurityUtils.getSubject();
         Map<String, String> user = new HashMap<>();
         user.put("userId", dbUser.getAccountCode());
         user.put("password", dbUser.getPassword());
         user.put("type", type);
-        Calendar instance = JwtUtils.getExpireTime(type.equals("account") ? 60 * 5 : 60 * 30);
-        String token = JwtUtils.generateToken(user, instance);
-        result.put("refreshToken", JwtUtils.generateToken(user, JwtUtils.getExpireTime(60 * 6)));
-        subject.login(new UsernamePasswordToken(userVO.getUserId(), token));
-        result.put("sessionId", getSessionId(subject));
-        result.put("accessToken", token);
-        result.put("userInfo", dbUser);
-        result.put("expireTime", instance.getTime());
-        return result;
+        subject.login(new UsernamePasswordToken(account, JwtUtils.generateToken(user)));
+        return getSessionId(subject);
     }
 
     private String getSessionId(Subject subject) {
