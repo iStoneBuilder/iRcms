090027b0df2a9fe1c8c1101660d471166fa30059
CJ.Chen <imaxstone@outlook.com>
2024-11-11 19:27:28 +0800

    权限 实现调整

--- micro.rcms.auth/src/main/java/com/stone/it/rcms/auth/dao/IAuthSettingDao.java
+++ micro.rcms.auth/src/main/java/com/stone/it/rcms/auth/dao/IAuthSettingDao.java
@@ -7,26 +7,27 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 /**
- *
  * @author cj.stone
  * @Date 2024/11/1
  * @Desc
  */
 public interface IAuthSettingDao {
 
-    List<AuthApisVO> findApiPathsByPaths(@Param("list") List<String> list);
+  List<AuthApisVO> findApiPathsByPaths(@Param("list") List<String> list);
 
-    void createApiPaths(@Param("list") List<AuthApisVO> list);
+  void createApiPaths(@Param("list") List<AuthApisVO> list);
 
-    void deleteApiPathsNotInList(@Param("list") List<String> permissionPathSet);
+  void deleteApiPathsNotInList(@Param("list") List<String> permissionPathSet);
 
-    void deleteApisRelationAuth();
+  void deleteApisRelationAuth();
 
-    AuthAccountVO getUserInfoByUserId(@Param("accountCode") String accountCode);
+  AuthAccountVO getUserInfoByUserId(@Param("accountCode") String accountCode);
 
-    List<AuthApisVO> getApiPathByRoleCodes(@Param("list") List<String> list);
+  List<AuthApisVO> getApiPathByRoleCodes(@Param("list") List<String> list);
 
-    void createSuperAdminAuth(@Param("list") List<String> list, @Param("roleCode") String roleCode);
+  void createSuperAdminAuth(@Param("list") List<String> list, @Param("roleCode") String roleCode);
 
-    ResDetailVO findAccountEnterpriseById(@Param("enterpriseId") String enterpriseId);
+  ResDetailVO findAccountEnterpriseById(@Param("enterpriseId") String enterpriseId);
+
+  List<String> findPermissionsByRoleList(@Param("list") List<String> roleList);
 }
