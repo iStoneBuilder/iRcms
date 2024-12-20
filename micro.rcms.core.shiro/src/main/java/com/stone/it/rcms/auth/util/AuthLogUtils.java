699b3593450c0b08aa04d300dd04bd263fdd7e2e
CJ.Chen <imaxstone@outlook.com>
2024-12-20 14:50:47 +0800

    获取用户登录地址信息成功

--- micro.rcms.core.shiro/src/main/java/com/stone/it/rcms/auth/util/AuthLogUtils.java
+++ micro.rcms.core.shiro/src/main/java/com/stone/it/rcms/auth/util/AuthLogUtils.java
@@ -3,6 +3,8 @@ package com.stone.it.rcms.auth.util;
 import com.alibaba.fastjson2.JSONObject;
 import com.stone.it.rcms.core.http.RequestUtil;
 import com.stone.it.rcms.core.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
@@ -14,25 +16,35 @@ import javax.servlet.http.HttpServletRequest;
  * @Desc
  */
 public class AuthLogUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthLogUtils.class);
+
     public static String getIpAddr(HttpServletRequest request) {
-        String ip = request.getHeader("X-Real-IP");
-        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("X-Forwarded-For");
+        String ipAddress = null;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP，多个IP按照','分割
+            if (ipAddress != null && ipAddress.length() > 15) {
+                // "***.***.***.***".length()
+                if (ipAddress.indexOf(",") > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("获取IP地址失败", e);
+            ipAddress = null;
         }
-        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
+        return ipAddress;
     }
 
     public static String getLocation(String ip) {
-        if (ip.contains("0:0:0:0")) {
+        if (ip == null || ip.contains("0:0:0:0")) {
             return "未知地点";
         }
         Map<String, String> params = new HashMap<>();
